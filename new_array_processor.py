071db426c26f4d23eec2391329fa5700ef7dbd95
Thomas Fletcher <fletchoc1@gmail.com>
2025-06-27 14:21:09 -0400

    New script for IR sensor

--- new_array_processor.py
+++ new_array_processor.py
@@ -1,10 +1,22 @@
 import serial
-
+import time
 #place in correct port(keep baud information)
-PORT = "COM10"
+PORT = "COM9"
 BAUD = 115200
 
 ser = serial.Serial(PORT, BAUD)
 
-def array_processor(ID):
-    
+def reader(ser):
+    while True: 
+        read = ser.readline()
+        try:
+            decode = read.decode('utf-8').strip()
+            print(decode)
+        except:
+            print("decode error")
+            continue
+def decoder(decode, ID):
+    temp = []
+    try: 
+        for i in range(len(decode)):
+            if 
