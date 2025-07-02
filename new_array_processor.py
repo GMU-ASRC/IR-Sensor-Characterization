8faf679ee14a9e653d97a325da54b92ac3db12d6
Thomas Fletcher <fletchoc1@gmail.com>
2025-07-02 14:01:06 -0400

    comments to understand pyserial

--- new_array_processor.py
+++ new_array_processor.py
@@ -2,23 +2,32 @@ import serial
 import time
 #place in correct port(keep baud information)
 PORT = "COM9"
-BAUD = 115200
+BAUD = 38400
 
 ser = serial.Serial(PORT, BAUD)
-
+#function to read the line from the serial terminal(takes data from serial terminal and decodes it into readable format)
 def reader(ser):
-    while True: 
-        read = ser.readline()
-        try:
+    try:
+        while True: 
+            #object for reading the binary data from serial terminal
+            read = ser.readline()
+            #decodes binary into readble format(string format)
             decode = read.decode('utf-8').strip()
-            print(decode)
-        except:
-            print("decode error")
+            #repeats indefinitly
+            decoder(decode)
             continue
-def decoder(decode, ID):
-    temp = []
-    try: 
-        temp = decode.split(",")
-        print(temp)
     except:
-           
+        print("decode error")
+#function to parce string data into an array(ignores anything that is not a letter or number)
+
+#start here: 
+def decoder(decode):
+    temp = []
+   
+    temp = decode.split(",")
+    for i in range(len(decode)):
+        if decode[i] == '[' or decode[i] == ']':
+            decode.pop(i)
+
+        print(temp)
+reader(ser)
